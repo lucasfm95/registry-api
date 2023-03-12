75b51d584fc9bc53cd6df9c354831c67e5131be1
Lucas Martins <martinslf@xcoreng.com>
2023-03-12 08:07:24 -0300

    adjustments in general

--- src/RegistryApi.Core/Services/Interfaces/ICustomerService.cs
+++ src/RegistryApi.Core/Services/Interfaces/ICustomerService.cs
@@ -1,11 +1,6 @@
-﻿using RegistryApi.Domain.Customers.Request;
+﻿using RegistryApi.Domain.Customers.Requests;
 using RegistryApi.Domain.Customers.Response;
 using RegistryApi.Domain.Request;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RegistryApi.Core.Services.Interfaces
 {
---
+++ src/RegistryApi.Domain/Customers/Requests/ProductPostRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistryApi.Domain.Customers.Requests
+{
+	public class ProductPostRequest
+	{
+		public int Id { get; set; }
+		public string? Model { get; set; }
+		public string? Description { get; set; }
+		public decimal Value { get; set; }
+        public bool? Enabled { get; set; }
+    }
+}
+
